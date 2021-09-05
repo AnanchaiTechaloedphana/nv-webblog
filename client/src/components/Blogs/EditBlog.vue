5dd717ad3a1410542ce61ffa704811c6ad4b8e61
AnanchaiTechaloedphana <ananchait62@nu.ac.th>
2021-09-05 22:51:23 +0700

    Ass

---
+++ client/src/components/Blogs/EditBlog.vue
@@ -0,0 +1,41 @@
+<template> 
+    <div>
+    <h1>Edit Blog</h1> 
+        <form v-on:submit.prevent = "editBlog">
+            <p>title: <input type="text" v-model="blog.title"></p> 
+            <p>content: <input type="text" v-model="blog.content"></p> 
+            <p>category: <input type="text" v-model="blog.category"></p> 
+            <p>status: <input type="text" v-model="blog.status"></p> 
+            <p><button type="submit">update blog</button>
+            <button V-on:click="navigateTo('/blogs')">กลับ</button> 
+            </p>
+        </form> 
+    </div>
+</template> 
+<script> import BlogsService from '@/services/BlogsService'
+
+    export default { 
+        data () { 
+            return { 
+                blog: {
+                title: '',
+                thumbnail: 'null', 
+                pictures: 'null', 
+                content: '', 
+                category: '', 
+                status: '', 
+                }
+            }
+        },
+    methods: { 
+        async editBlog () {
+             try { 
+                 await BlogsService.put(this.blog) 
+                 this.$router.push({
+            name: 'blogs'
+})
+} catch (err) {
+console.log(err)
+}
+        }
+        
